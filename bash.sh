5f94a33cf7a72694c983892d91a942a40eec9f70
brenthaertlein <brenthaertlein@gmail.com>
2018-08-11 10:49:08 -0400

    Yes, I was being sarcastic.

--- bash.sh
+++ bash.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+export HISTCONTROL=ignoreboth
+export HISTCONTROL="frequent*:cd -"
+export HISTORY_IGNORE="(frequent*|cd -)"
+
 alias ...=../..
 alias ....=../../..
+
+fn_freq() {
+  local num=${1:-10}
+	cat $HISTFILE | sed -E 's/\:\s(.*)\:(.)\;//g' | awk '{print $1}' | sort | uniq -c | sort -nr | head -n $num
+  #history | awk 'BEGIN {FS="[ \t]+|\\|"} {print $3}' | sort | uniq -c | sort -nr | head -n $num
+}
+alias frequent=fn_freq $@
+
